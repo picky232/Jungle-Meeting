c23170b8b082afb191aa9501fe725a1de5a614c0
dongho <jijiji0809160@gmail.com>
2026-08-27 04:25:19 +0900

    save

--- backend/app.py
+++ backend/app.py
@@ -480,6 +480,10 @@ def logout():
     response.delete_cookie('access_token')
 
     return response
-
+    
 if __name__ == '__main__':
-    app.run(debug=True, port=3000)
+    app.run(
+        host='0.0.0.0',
+        port=3000,
+        debug=True
+    )
